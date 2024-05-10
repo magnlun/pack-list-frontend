93d9cdde9f24ba1ca53c4b6ba380e5677d8efd33
Magnus Lundberg <ludde89l@gmail.com>
2024-05-10 22:53:00 +0200

    Fixed the pack list page on desktop

--- src/app/components/checkbox/checkbox.component.ts
+++ src/app/components/checkbox/checkbox.component.ts
@@ -2,7 +2,8 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox',
-  templateUrl: './checkbox.component.html'
+  templateUrl: './checkbox.component.html',
+  styleUrls: ['./checkbox.component.scss']
 })
 export class CheckboxComponent {
   @Input()
